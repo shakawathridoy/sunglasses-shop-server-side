9ca5193a1e872eb439c8f5871aa8e6c4b22105a4
shakawathridoy <contact.freelancerhridoy@gmail.com>
2021-11-19 17:55:54 +0600

    3rdd

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const cors = require('cors')
 require('dotenv').config()
 const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
-
+const ObjectId = require("mongodb").ObjectId;
+// const { restart } = require('nodemon');
+const bodyParser = require('body-parser');
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cors());
 
@@ -12,16 +16,76 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run(){
-    try{
-        await client.connect();
-        console.log('database connected batija');
-    }
-    finally{
-        // await client.close()
-    }
-}
-run().catch(console.dir);
+client.connect((err) => {
+  const sunglassesCollection = client.db("sunglassesWorld").collection("sunglasses");
+  const orderCollection = client.db("sunglassesWorld").collection("orders");
+
+  // add place
+
+  app.post("/addNew", async (req, res)=> {
+      const result = await sunglassesCollection.insertOne(req.body)
+      res.send(result);
+  });
+
+  app.get("/products", async (req, res) => {
+      const result = await sunglassesCollection.find({}).toArray();
+      res.send(result)
+  })
+
+  // get single place 
+  app.get("/singleProduct/:id", async (req, res) => {
+    console.log(req.body);
+      const result = await sunglassesCollection
+      .find({_id: ObjectId(req.params.id)})
+      .toArray();
+      res.send(result[0]);
+  });
+
+
+  // confirm Order
+  app.post("/confirmOrder", async (req, res) => {
+      const result = await orderCollection.insertOne(req.body)
+      res.send(result);
+  });
+
+
+  // // My Order
+  // app.get("/myBookings/:email", async (req, res) => {
+  //     const result = await bookingCollection.find({ email: req.params.email}).toArray();
+  //     res.send(result);
+  // });
+
+
+  // // Cancel Order 
+  // app.delete("/cancelOrder/:id", async (req, res) => {
+  //     const result = await bookingCollection.deleteOne({_id: ObjectId(req.params.id)
+  //     });
+  //     res.send(result);
+  // })
+
+  // // All Bookings
+
+  // app.get("/allBooking", async (req, res) => {
+  //     const result = await bookingCollection.find({}).toArray()
+  //     res.send(result);
+  // })
+
+  // // update Status 
+  // app.put("/updateStatus/:id", (req, res) => {
+  //     const id = req.params.id;
+  //     const updatedStatus = req.body.status;
+  //     const filter = {_id: ObjectId(id)};
+  //      bookingCollection.updateOne(filter, {
+  //         $set: {status: updatedStatus},
+  //     })
+  //     .then(result => {
+  //         res.send(result);
+  //     })
+      
+  // })
+
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello Sellers, How are you doing!')
@@ -30,3 +94,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at ${port}`)
 })
+
+// app.listen(process.env.PORT || port)
